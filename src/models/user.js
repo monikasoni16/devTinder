4bdaa50e3e254dfdb87aed57e2ea8115ee385fc3
Monika <soni.monika1692@gmail.com>
2026-05-30 00:51:56 +0530

    added validations with validator.js library

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 const userSchema = new mongoose.Schema({
     firstName: {
@@ -15,10 +16,20 @@ const userSchema = new mongoose.Schema({
         unique: true,
         lowercase: true,
         trim: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error("Invalid email");
+            }
+        }
     },
     password: {
         type: String,
         required: true,
+        validate(value) {
+            if (!validator.isStrongPassword(value)) {
+                throw new Error("Enter strong password");
+            }
+        }
     },
     age: {
         type: Number,
@@ -35,6 +46,11 @@ const userSchema = new mongoose.Schema({
     photoUrl: {
         type: String,
         default: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png",
+        validate(value) {
+            if (!validator.isURL(value)) {
+                throw new Error("Invalid photo URL");
+            }
+        }
     },
     about: {
         type: String,
